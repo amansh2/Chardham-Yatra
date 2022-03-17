0f5c9d3e4f1377ceb13cc6c15b9438f9ade39aab
AD <amanshdeep2@gmail.com>
2022-03-17 16:43:00 +0530

    initial commit

---
+++ app/src/main/java/com/example/chardhamyatra/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.chardhamyatra
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//
+//
+//        val uri =
+//            Uri.parse("geo:29.941465620499056, 76.81746399686475?q=hotels")
+//        val intent= Intent(Intent.ACTION_VIEW,uri)
+//        intent.setPackage("com.google.android.apps.maps")
+//        startActivity(intent)
+    }
+}
