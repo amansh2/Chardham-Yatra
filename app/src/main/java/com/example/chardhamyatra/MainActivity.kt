94ac738831b0021e3c780bf3400aca2a38ca23cf
AD <amanshdeep2@gmail.com>
2022-03-23 11:18:46 +0530

    initial commit

--- app/src/main/java/com/example/chardhamyatra/MainActivity.kt
+++ app/src/main/java/com/example/chardhamyatra/MainActivity.kt
@@ -4,11 +4,51 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.chardhamyatra.databinding.ActivityMainBinding
+import com.example.eventmanagement.AuthAppRepository
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var repository:AuthAppRepository
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        repository= AuthAppRepository(application)
+        repository.getUserLiveData().observe(this,{
+            if(it!=null){
+                val intent= Intent(this,NavigationHostActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        })
+
+
+        binding.apply {
+
+            login.setOnClickListener {
+                val e:String=email.text.toString()
+                val p:String=password.text.toString()
+                if(e.isNotEmpty() && p.isNotEmpty()){
+                    repository.login(e,p)
+                }
+                else{
+                    Toast.makeText(application.applicationContext,"Email Address And Password Must Be Entered",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+            register.setOnClickListener {
+               startActivity(Intent(this@MainActivity,RegisterActivity::class.java))
+            }
+
+        }
+
+
+
+
+
 //
 //
 //        val uri =
@@ -17,4 +57,6 @@ class MainActivity : AppCompatActivity() {
 //        intent.setPackage("com.google.android.apps.maps")
 //        startActivity(intent)
     }
+
+
 }
